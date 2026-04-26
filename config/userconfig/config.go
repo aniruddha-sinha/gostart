f7fb36ca7a3da7a76c738beb53a2bd907eacdb6d
Aniruddha Sinha <asinha0493@gmail.com>
2026-04-27 02:43:47 +0530

    made the env self contained; removed viper env since viper env does not work with binary

--- config/userconfig/config.go
+++ config/userconfig/config.go
@@ -1,31 +1,37 @@
 package userconfig
 
 import (
+	"bytes"
+	"embed"
 	"fmt"
 
 	"github.com/spf13/viper"
 )
 
+//go:embed defaults.yaml
+var defaultConfigFile embed.FS
+
 type AppConfig struct {
-	MiseGoPath      string `mapstructure:"MISE_GO_PATH"`
-	DefaultFilePerm string `mapstructure:"DEFAULT_FILE_PERMISSION"`
+	MiseGoPath      string `mapstructure:"mise_go_path"`
+	DefaultFilePerm string `mapstructure:"default_file_permission"`
 }
 
 func LoadConfig() (AppConfig, error) {
 	var config AppConfig
 
+	defaultBytes, err := defaultConfigFile.ReadFile("defaults.yaml")
+	if err != nil {
+		return config, fmt.Errorf("failed to read embedded defaults: %w", err)
+	}
+
+	viper.SetConfigType("yaml")
+	if err := viper.ReadConfig(bytes.NewReader(defaultBytes)); err != nil {
+		return config, fmt.Errorf("failed to parse embedded defaults: %w", err)
+	}
+
 	viper.AutomaticEnv()
-
-	if err := viper.BindEnv("MISE_GO_PATH"); err != nil {
-		return AppConfig{}, fmt.Errorf("error reading config %v", err)
-	}
-
-	if err := viper.BindEnv("DEFAULT_FILE_PERMISSION"); err != nil {
-		return AppConfig{}, fmt.Errorf("error reading config %v", err)
-	}
-
 	if err := viper.Unmarshal(&config); err != nil {
-		return config, fmt.Errorf("failed to unmarshal env config: %w", err)
+		return config, fmt.Errorf("failed to unmarshal config: %w", err)
 	}
 
 	return config, nil
